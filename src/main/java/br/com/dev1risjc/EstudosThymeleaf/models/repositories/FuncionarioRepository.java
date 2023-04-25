5247e59c6a278f1efaf3cf430be39dd770186fcc
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-04-25 15:47:48 -0300

    Delete src directory

--- src/main/java/br/com/dev1risjc/EstudosThymeleaf/models/repositories/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.dev1risjc.EstudosThymeleaf.models.repositories;
-
-import br.com.dev1risjc.EstudosThymeleaf.models.entities.Funcionario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
-}
