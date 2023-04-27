f64e075c8b8b9bad54f477377ef1a5d9a3fa8910
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-04-27 16:56:15 -0300

    Update FuncionarioController.java

--- src/main/java/br/com/dev1risjc/EstudosThymeleaf/controllers/FuncionarioController.java
+++ src/main/java/br/com/dev1risjc/EstudosThymeleaf/controllers/FuncionarioController.java
@@ -1,12 +1,18 @@
 package br.com.dev1risjc.EstudosThymeleaf.controllers;
 
+import br.com.dev1risjc.EstudosThymeleaf.models.entities.Cargo;
 import br.com.dev1risjc.EstudosThymeleaf.models.entities.Funcionario;
+import br.com.dev1risjc.EstudosThymeleaf.models.entities.UF;
+import br.com.dev1risjc.EstudosThymeleaf.models.repositories.CargoRepository;
 import br.com.dev1risjc.EstudosThymeleaf.models.repositories.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -17,21 +23,26 @@ public class FuncionarioController {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
+    @Autowired
+    private CargoRepository cargoRepository;
+
     @GetMapping("/cadastrar")
-    public String cadastrar() {
+    public String cadastrar(Funcionario funcionario) {
         return "funcionarios/cadastro";
     }
 
     @GetMapping("/listar")
-    public String listar() {
-        return "funcionarios/lista";
+    public String listar(ModelMap modelMap) {
+        modelMap.addAttribute("funcionarios", (List<Funcionario>)funcionarioRepository.findAll());
+        return "/funcionarios/lista";
     }
 
     @PostMapping("/novoFuncionario")
     @Transactional(readOnly = false)
-    public @ResponseBody Funcionario novoFuncionario(Funcionario Funcionario) {
+    public String novoFuncionario(Funcionario Funcionario, RedirectAttributes attributes) {
         funcionarioRepository.save(Funcionario);
-        return Funcionario;
+        attributes.addFlashAttribute("sucesso", "Funcionário inserido com sucesso");
+        return "redirect:/funcionarios/cadastrar";
     }
 
     @PutMapping("/editar")
@@ -57,5 +68,15 @@ public class FuncionarioController {
     @Transactional(readOnly = false)
     public void deletar(@PathVariable int id) {
         funcionarioRepository.deleteById(id);
+    }
+
+    @ModelAttribute("cargos")
+    public List<Cargo> getCargos() {
+        return (List<Cargo>) cargoRepository.findAll();
+    }
+
+    @ModelAttribute("ufs")
+    public UF[] getUFs() {
+        return UF.values();
     }
 }
