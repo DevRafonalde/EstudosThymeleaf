0e8415faefb7f3b1cc0bc4c90d89ad953284bc01
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-05-02 16:55:09 -0300

    Delete src directory

--- src/main/java/br/com/dev1risjc/EstudosThymeleaf/models/repositories/DepartamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.dev1risjc.EstudosThymeleaf.models.repositories;
-
-import br.com.dev1risjc.EstudosThymeleaf.models.entities.Departamento;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DepartamentoRepository extends CrudRepository<Departamento, Integer> {
-}
